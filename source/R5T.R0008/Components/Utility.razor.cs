1dfb2371c6ae46e0721c70b3e14b8644b3800d49
davidcoats <david@getsafetycone.com>
2023-08-12 23:23:26 -0700

    Interim changes.

--- source/R5T.R0008/Components/Utility.razor.cs
+++ source/R5T.R0008/Components/Utility.razor.cs
@@ -1,11 +1,14 @@
 using System;
 
+using R5T.T0154;
+
 
 namespace R5T.R0008
 {
     /// <summary>
     /// Razor component that allows using the Razor editor to provide render fragments in C# code.
     /// </summary>
+    [RazorComponentMarker]
     public partial class Utility
     {
 
